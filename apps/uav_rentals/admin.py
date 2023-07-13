0b141502990fc486f5106fd3f4b4d4308a964f83
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-13 23:49:51 +0300

    added admin classes for uav_rentals app

--- apps/uav_rentals/admin.py
+++ apps/uav_rentals/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.uav_rentals.models import *
+
 
 # Register your models here.
+@admin.register(UAVRental)
+class UAVRentalAdmin(admin.ModelAdmin):
+    list_display = ["id", "uav", "renter", "created_at", "updated_at"]
+    list_filter = ["renter", "created_at", "updated_at"]
