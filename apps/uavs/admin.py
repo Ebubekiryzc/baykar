77725087e5ab4ad83a45e6cd829976302e806f52
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-13 23:49:59 +0300

    added admin classes for uavs app

--- apps/uavs/admin.py
+++ apps/uavs/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from apps.shared.admin import ImageInline
+from apps.uavs.models import *
+from mptt.admin import DraggableMPTTAdmin
+
 
 # Register your models here.
+@admin.register(UAV)
+class UAVAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "slug",
+        "uav_manifacturer",
+        "weight",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["uav_manifacturer", "uav_category", "created_at", "updated_at"]
+    search_fields = ["name", "slug"]
+    inlines = [ImageInline]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
+
+
+@admin.register(UAVCategory)
+class UAVCategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "slug", "created_at", "updated_at"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["name", "slug"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
+
+
+@admin.register(UAVManifacturer)
+class UAVManifacturerAdmin(DraggableMPTTAdmin):
+    list_display = [
+        "tree_actions",
+        "indented_title",
+        "id",
+        "name",
+        "slug",
+    ]
+    list_display_links = ["indented_title"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["name", "slug"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
