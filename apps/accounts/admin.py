2b1080eceb3967c0b1860507138cb868a69ba48c
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-13 23:41:37 +0300

    added admin classes for accounts app

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from apps.accounts.models import *
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["email", "username"]
