0d9f81921cc04dba8ea263fb2450403c5d57239c
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-14 15:17:50 +0300

    refactored

--- apps/uavs/api/urls.py
+++ apps/uavs/api/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import *
+from apps.uavs.api.views import *
 
 router = DefaultRouter()
 router.register(r"uavs", UAVViewSet, basename="uav")
 router.register(r"uav-categories", UAVCategoryViewSet, basename="uav-category")
-router.register(r"uav-manifacturers", UAVManifacturerViewSet, basename="uav-manifacturer")
+router.register(
+    r"uav-manifacturers", UAVManifacturerViewSet, basename="uav-manifacturer"
+)
 
-urlpatterns = [path("api/", include(router.urls))]
+urlpatterns = [path("", include(router.urls))]
