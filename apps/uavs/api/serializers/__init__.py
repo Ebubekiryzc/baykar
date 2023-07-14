72a94cd06174fbdd402a3dbf2b9c267c2e6a2acf
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-14 15:16:42 +0300

    changed priorities for imports

--- apps/uavs/api/serializers/__init__.py
+++ apps/uavs/api/serializers/__init__.py
@@ -1,3 +1,3 @@
-from .uav_serializer import UAVSerializer
 from .uav_category_serializer import UAVCategorySerializer
 from .uav_manifacturer_serializer import UAVManifacturerSerializer
+from .uav_serializer import UAVSerializer
